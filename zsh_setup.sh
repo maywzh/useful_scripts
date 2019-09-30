9fe91d6e076b51304aeadb20b3e01d1a395d0a8d
maywzh <maywzh@gmail.com>
2019-09-30 18:34:59 +0800

    zsh SETUP scripts

---
+++ zsh_setup.sh
@@ -0,0 +1,6 @@
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/sobolevn/wakatime-zsh-plugin.git $ZSH_CUSTOM/plugins/wakatime-zsh-plugin
+echo "zsh-autosuggestions zsh-syntax-highlighting wakatime"
